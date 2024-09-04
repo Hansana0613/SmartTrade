5c5a72894de1fb4aae2151db92b1bc4adc338fff
Hansana Madhuranga <153816119+Hansana0613@users.noreply.github.com>
2024-09-04 12:18:35 +0530

    dasfd

--- src/java/controller/SignUp.java
+++ src/java/controller/SignUp.java
@@ -1,7 +1,9 @@
 package controller;
 
+import org.hibernate.Session;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dto.Response_DTO;
 import dto.User_DTO;
 import entity.User;
 import java.io.IOException;
@@ -10,6 +12,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import model.Validations;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -21,23 +27,45 @@ public class SignUp extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        Response_DTO response_DTO = new Response_DTO();
+
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
         User_DTO user_DTO = gson.fromJson(request.getReader(), User_DTO.class);
 
         if (user_DTO.getFirst_name().isEmpty()) {
-
+            response_DTO.setContent("Please Enter Your First Name");
         } else if (user_DTO.getLast_name().isEmpty()) {
-
+            response_DTO.setContent("Please Enter Your Last Name");
         } else if (user_DTO.getEmail().isEmpty()) {
-
-        } else if (user_DTO.getEmail().) {
-
+            response_DTO.setContent("Please Enter Your Email");
+        } else if (!Validations.isEmailValid(user_DTO.getEmail())) {
+            response_DTO.setContent("Please Enter a Valid Email");
         } else if (user_DTO.getPassword().isEmpty()) {
-
-        } else if (user_DTO.getPassword().) {
-
+            response_DTO.setContent("Please Enter Your Password");
+        } else if (Validations.isPasswordValid(user_DTO.getPassword())) {
+            response_DTO.setContent("Password must contain at least one uppercase letter, "
+                    + "number, special character and be at least eight characters long");
         } else {
+
+            Session session = HibernateUtil.getSessionFactory().openSession();
+
+            Criteria criteria = session.createCriteria(User.class);
+            criteria.add(Restrictions.eq("email", user_DTO.getEmail()));
+
+            if (!criteria.list().isEmpty()) {
+                response_DTO.setContent("User with this Email already exists");
+            } else {
+
+                //generate verification code
+                
+                User user = new User();
+
+                session.save(user);
+
+                //send verification email
+            }
+            session.close();
 
         }
 
